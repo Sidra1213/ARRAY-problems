5fa8b8cca2226154988cf12e59f067930c129b72
Sidra sajjad <sidrasajjad0987@gmail.com>
2026-09-18 22:59:11 +0500

    Add files via upload

---
+++ task 1.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+//task 1 plus task 2 
+
+int main()
+{
+string name,
+string degree;
+int age,semester;
+float cgpa;
+
+cout<<"enter your name : "<<endl;
+cin>>name;
+cout<<"enter your age : "<<endl;
+cin>>age;
+cout<<"enter your cgpa : ";
+cin>>cgpa;
+cout<<"enter your semester : "<<endl;
+cin>>semester;
+cout<<"enter your degree : "<<endl;
+cin>>degree;
+
+
+cout<<"-----your information-----"<<endl;
+cout<<"your name is : "<<name<<endl;
+cout<<"your age is : "<<age<<endl;
+cout<<"your cgpa is : "<<cgpa<<endl;
+cout<<"your semester is : "<<semester<<endl;
+cout<<"your degree is : "<<degree<<endl;
+return 0;
+
+}
